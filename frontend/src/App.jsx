6632d24cd848d24ec35917a2ae39006ee826d4d8
RetanoProx <retanoprox@gmail.com>
2025-08-31 01:49:35 +0300

    0.19

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AuthScreen from './auth/AuthScreen';
+import AuthScreen from './components/AuthScreen';
 
 function App() {
   return (
